6fcb4d63098822214ef6c41242fdcbfd3f630c6d
Mark Delos Reyes <makizworks@gmail.com>
2017-10-04 10:25:40 -0400

    test touchend remove touchstart

--- js/script.js
+++ js/script.js
@@ -2,15 +2,15 @@ let modal = document.querySelector('.mymodal');
 let modalBtn = document.querySelector('#modalBtn');
 let closeBtn = document.querySelector('.close-btn');
 
-function touchstart(e) {
+function touchend(e) {
     e.preventDefault();
     return false;
 }
 
-modalBtn.addEventListener('click', openModal);
-modalBtn.addEventListener('touchstart', touchstart);
+modalBtn.addEventListener('click', openModal, false);
+modalBtn.addEventListener('touchend', touchend, false);
 
-closeBtn.addEventListener('click', closeModal);
+closeBtn.addEventListener('click', closeModal, false);
 
 function openModal() {
     modal.style.display = 'block';
