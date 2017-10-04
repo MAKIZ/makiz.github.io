5ef15efc18e54c4f4e5c115640dc0febc6918313
Mark Delos Reyes <makizworks@gmail.com>
2017-10-03 23:40:13 -0400

    add modal

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let modal = document.querySelector('.mymodal');
+let modalBtn = document.querySelector('#modalBtn');
+let closeBtn = document.querySelector('.close-btn');
+
+modalBtn.addEventListener('click', openModal);
+closeBtn.addEventListener('click', closeModal);
+
+function openModal() {
+    modal.style.display = 'block';
+    modal.style.transition = "height 5s linear 0s";
+    modal.style.height = "100%";
+}
+
+function closeModal() {
+    modal.style.display = 'none';
+}
